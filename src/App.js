06e70722ec8a8cb4b165d5d162bc46289ce27aa8
shahd <rtshahd84@gmail.com>
2026-06-16 00:07:58 +0200

    initial commit -todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
-  return (
+
+  const [todos,settodos] = useState([])
+  const inputRef=useRef()
+  const handleAdd=()=>{
+    const text = inputRef.current.value
+    const NewItem={
+      complete:false,
+      text:text
+    }
+    settodos([...todos,NewItem])
+    inputRef.current.value=""
+    console.log(todos)
+  }
+  const handleDone=(index)=>{
+    const newtodos=[...todos]
+    newtodos[index].complete=!newtodos[index].complete
+    settodos(newtodos)
+    // console.log(newtodos[index].complete)
+    console.log(todos)
+  }
+
+  const handleDelete=(index)=>{
+    const newtodos=[...todos]
+    newtodos.splice(index,1)
+    settodos(newtodos)
+  }
+
+  
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>TODO List</h2>
+     <ul>
+      {todos.map((item, index) => {
+      return (
+      <li key={index} className='container-item'>
+        
+        <span
+          className={item.complete ? "done" : ""}
+          onClick={() => handleDone(index)}
         >
-          Learn React
-        </a>
-      </header>
+          {item.text}
+        </span>
+
+        <span className='cross' onClick={() => handleDelete(index)}>
+          ❌
+        </span>
+
+      </li>
+       );
+     })}
+    </ul>
+      <div className="input-group">
+        <input ref={inputRef} placeholder='....Enter Item'></input>
+      <button onClick={handleAdd}>Add</button>
+      </div>
+
+
+
+
+
     </div>
-  );
+ )
 }
 
 export default App;
